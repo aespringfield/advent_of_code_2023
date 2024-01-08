e52326f69b94993ac4362de44bd57d39e215ea1f
Anna Springfield <anna.springfield@wonolo.com>
2024-01-08 00:03:52 -0600

    Day 14 part 2

--- 14.rb
+++ 14.rb
@@ -1,4 +1,18 @@
 module Day14
+  DIRECTIONS = {
+    north: 'north',
+    east: 'east',
+    south: 'south',
+    west: 'west'
+  }.freeze
+
+  DIRECTIONS_ORDER = [
+    DIRECTIONS[:north],
+    DIRECTIONS[:west],
+    DIRECTIONS[:south],
+    DIRECTIONS[:east]
+  ].freeze
+
   class << self
     def part_one(input)
       # Transpose input so that columns are rows
@@ -23,8 +37,82 @@ module Day14
       # Sum rows
     end
 
-    def part_two(input)
-      raise NotImplementedError
+    def part_two(input, tilts_count = 1_000_000_000 * 4)
+      cache = {}
+
+      # Clean input
+      lines = Input.new(raw_lines: input).clean
+
+      # For each i in tilts_count:
+      tilts_count.times do |i|
+        direction = DIRECTIONS_ORDER[i % DIRECTIONS_ORDER.count]
+
+        # Tilt, get sum
+        lines = tilt(lines, direction)
+
+        # Check cache for dir + lines
+        if cache[[direction, lines]]
+
+          # If in cache, count tilts between that entry and now
+          cycle_length = i - cache[[direction, lines]]
+
+          # Return total load in cycle where remaining tilts % cycle length is cycle index
+          cycle_index = (tilts_count - (i + 1)) % cycle_length
+          cycle_stage_lines = cache.key(cycle_index + cache[[direction, lines]])[1]
+
+          return calculate_total_load(cycle_stage_lines)
+        else
+          # If not in cache, add to cache, where key is dir + lines and value is index
+          cache[[direction, lines]] = i
+        end
+      end
+    end
+
+    def calculate_total_load(lines)
+      lines.map.with_index { |line, i| line.count('O') * (lines.count - i) }.sum
+    end
+
+    def tilt(lines, direction)
+      cache = {}
+
+      lines.then do |original_lines|
+        # Transpose if dir is north or south
+        [DIRECTIONS[:north], DIRECTIONS[:south]].include?(direction) ? original_lines.transpose.map(&:reverse) : original_lines
+      end.map do |line|
+        segments_by_end_index = {}
+        # For each row:
+        #   Segment row by # characters, recording start and end index
+        line.join.scan(/#+|[^#]+/) do
+          end_index = $~.offset(0)[1] - 1
+          segments_by_end_index[end_index] = $&
+        end
+
+        segments = segments_by_end_index.each_pair.sort_by(&:first).map { |pair| pair[1] }
+
+        segments.flat_map do |segment|
+          # Check cache for segment
+          next cache[[direction, segment]] if cache[[direction, segment]]
+
+          result = if segment.match(/^#+$/)
+            segment.split('')
+          else
+            rolling_rock_count = segment.scan(/O/)&.count
+            nothing_count = segment.length - rolling_rock_count
+
+            if [DIRECTIONS[:south], DIRECTIONS[:west]].include?(direction)
+              Array.new(rolling_rock_count, 'O') + Array.new(nothing_count, '.')
+            else
+              Array.new(nothing_count, '.') + Array.new(rolling_rock_count, 'O')
+            end
+          end
+
+          cache[[direction, segment]] = result
+          result
+        end
+      end.then do |new_lines|
+        # Transpose back if dir is north or south
+        [DIRECTIONS[:north], DIRECTIONS[:south]].include?(direction) ? new_lines.transpose.reverse : new_lines
+      end
     end
   end
 
--- spec/14_spec.rb
+++ spec/14_spec.rb
@@ -1,13 +1,90 @@
-describe 'Day13' do
+describe 'Day14' do
   describe '.part_one' do
-    it 'produces the correct output for input' do
-      expect(Day14.part_one(@input)).to eq(@part_one_expected)
+    it 'produces the correct output for input A' do
+      expect(Day14.part_one(@input_A)).to eq(@part_one_expected_A)
     end
   end
 
   describe '.part_two' do
-    it 'produces the correct output for input' do
-      expect(Day14.part_two(@input)).to eq(@part_two_expected)
+    it 'produces the correct output for input A' do
+      expect(Day14.part_two(@input_A)).to eq(@part_two_expected_A)
+    end
+
+    it 'produces the correct output for input B' do
+      expect(Day14.part_two(@input_B)).to eq(@part_two_expected_B)
+    end
+  end
+
+  describe '.calculate_total_load' do
+    it 'returns 0 when lines empty' do
+      expect(Day14.calculate_total_load([])).to eq(0)
+    end
+
+    it 'returns correct load for single line' do
+      expect(Day14.calculate_total_load([%w[O O . # O .]])).to eq(3)
+    end
+
+    it 'returns correct load for multiple lines' do
+      expect(Day14.calculate_total_load([%w[O O . # O .], %w[. . O # O .]])).to eq(8)
+    end
+
+    it 'returns correct load for a bunch of lines' do
+      lines = [
+        %w[. . . . . # . . . .],
+        %w[. . . . # . . . O #],
+        %w[. . . . . # # . . .],
+        %w[. . . # . . . . . .],
+        %w[. . . . . O O O # .],
+        %w[. O # . . . O # . #],
+        %w[. . . . O # . . . O],
+        %w[. . . . . . O O O O],
+        %w[# . . . O # # # . O],
+        %w[# . . O O # . . O O]
+      ]
+
+      expect(Day14.calculate_total_load(lines)).to eq(65)
+    end
+  end
+
+  describe '.tilt' do
+    let(:lines) do
+      [
+        %w[ . . O . # . O # O],
+        %w[ O . O # # . # . .],
+        %w[ . O . # . O . O #]
+      ]
+    end
+
+    it 'tilts east' do
+      expect(Day14.tilt(lines, Day14::DIRECTIONS[:east])).to eq([
+        %w[ . . . O # . O # O],
+        %w[ . O O # # . # . .],
+        %w[ . . O # . . O O #],
+      ])
+    end
+
+    it 'tilts west' do
+      expect(Day14.tilt(lines, Day14::DIRECTIONS[:west])).to eq([
+        %w[ O . . . # O . # O],
+        %w[ O O . # # . # . .],
+        %w[ O . . # O O . . #],
+      ])
+    end
+
+    it 'tilts south' do
+      expect(Day14.tilt(lines, Day14::DIRECTIONS[:south])).to eq([
+        %w[ . . . . # . O # .],
+        %w[ . . O # # . # . O],
+        %w[ O O O # . O . O #]
+      ])
+    end
+
+    it 'tilts north' do
+      expect(Day14.tilt(lines, Day14::DIRECTIONS[:north])).to eq([
+        %w[ O O O . # O O # O],
+        %w[ . . O # # . # O .],
+        %w[ . . . # . . . . #]
+      ])
     end
   end
 
